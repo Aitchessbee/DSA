77da2cc6f43e0fb977ad317608e5d2eb9be38fc1
Aitchessbee <bediharsiddak@gmail.com>
2022-07-09 15:27:31 +0530

    random codes

---
+++ leetcode/191.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    uint32_t n;
+    cin >> n;
+
+    // int count = 0;
+    // while(n!=0) {
+    //     int digit = n%10;
+    //     if(digit == 1){
+    //         count += 1;
+    //     }
+    //     n/=10;
+    // }
+    // cout << count;
+
+    cout << n;
+}
--- random codes/decimal_to_binary.cpp
+++ random codes/decimal_to_binary.cpp
@@ -9,10 +9,17 @@ int main(){
     int i=0;
     int ans=0;
 
+    // while(n!=0){
+    //     int bit = n&1;
+    //     ans += (bit * pow(10,i));
+    //     n = n >> 1;
+    //     i++;
+    // }
+
     while(n!=0){
-        int bit = n&1;
+        int bit = n%2;
         ans += (bit * pow(10,i));
-        n = n >> 1;
+        n = n / 2;
         i++;
     }
 
